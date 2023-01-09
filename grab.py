afb47727e9a63b248c66a4248438e1856793de21
TvoiSenpai12 <sobolev.shurik@mail.ru>
2023-01-09 07:22:16 +0300

    Added client Telethon

--- grab.py
+++ grab.py
@@ -1,1 +1,7 @@
-print('Hello test')
+from telethon import TelegramClient, events
+# Тут вставляй свои данные с https://my.telegram.org/apps
+api_id = '16851554'
+api_hash = '46e14bf8998f41ca3d889bcf430d4cef'
+
+client = TelegramClient('anon', api_id, api_hash)
+client.start()

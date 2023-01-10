56821b95ff844ece044a92785205cdddb291c9a9
TvoiSenpai12 <sobolev.shurik@mail.ru>
2023-01-10 17:14:27 +0300

    Added comments

--- grab.py
+++ grab.py
@@ -6,9 +6,11 @@ api_hash = '46e14bf8998f41ca3d889bcf430d4cef'
 client = TelegramClient('anoni', api_id, api_hash)
 client.start()
 
-@client.on(events.NewMessage(-1001708158788))
+#откуда пиздим
+@client.on(events.NewMessage(-1001708158788)) 
 
+#туда срем
 async def main(event):
-    await client.forward_messages(-1001771993154, event.message)
+    await client.send_message(-1001771993154, event.message)
     
 client.run_until_disconnected()

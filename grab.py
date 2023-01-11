18ca9c046417caca9c445efac0a52f9b8d3d6a07
TvoiSenpai12 <sobolev.shurik@mail.ru>
2023-01-11 13:46:45 +0300

    Added new grabber

--- grab.py
+++ grab.py
@@ -1,16 +1,87 @@
-from telethon import TelegramClient, events
-# Тут вставляй свои данные с https://my.telegram.org/apps
-api_id = '16851554'
-api_hash = '46e14bf8998f41ca3d889bcf430d4cef'
+from telethon import TelegramClient, events, errors
+import asyncio
+import re
+# ----
+api_id = 16851554
+api_hash = "46e14bf8998f41ca3d889bcf430d4cef"
+# ----
+channels = '@NewGameProject'  # откуда
+my_channel = '@mamoru_xui'  # куда
+# -----
+KEYS = {
+    "ссылка": ''
+}
+# ----
+Bad_Keys = ['Будь в курсе всех раздач', 'быть в курсе', 'Подписывайся, чтобы', 'Условия участия', '#промо']
+# ----
+tags = ''
+# добавление текста к посту, если не надо оставить ковычки пустыми ""
+# ----
+with TelegramClient('myApp13', api_id, api_hash) as client:
+    print("～Activated～")
 
-client = TelegramClient('anoni', api_id, api_hash)
-client.start()
+    @client.on(events.NewMessage(chats=channels))
+    async def Messages(event):
+        if not [element for element in Bad_Keys
+                if event.raw_text.lower().__contains__(element)]:
+            text = event.raw_text
+            for i in KEYS:
+                text = re.sub(i, KEYS[i], text)
+            if not event.grouped_id\
+                    and not event.message.forward:
+                try:
+                    await client.send_message(
+                        entity=my_channel,
+                        file=event.message.media,
+                        message=text + tags,
+                        parse_mode='md',
+                        link_preview=False)
+                except errors.FloodWaitError as e:
+                    print(f'[!] Ошибка флуда ждем: {e.seconds} секунд')
+                    await asyncio.sleep(e.seconds)
+                except Exception as e:
+                    print('[!] Ошибка', e)
+            elif event.message.text and not event.message.media\
+                and not event.message.forward\
+                    and not event.grouped_id:
+                try:
+                    await client.send_message(
+                        entity=my_channel,
+                        message=text + tags,
+                        parse_mode='md',
+                        link_preview=False)
+                except errors.FloodWaitError as e:
+                    print(f'[!] Ошибка флуда ждем: {e.seconds} секунд')
+                    await asyncio.sleep(e.seconds)
+                except Exception as e:
+                    print('[!] Ошибка', e)
+            elif event.message.forward:
+                try:
+                    await event.message.forward_to(my_channel)
+                except errors.FloodWaitError as e:
+                    print(f'[!] Ошибка флуда ждем: {e.seconds} секунд')
+                except Exception as e:
+                    print('[!] Ошибка', e)
 
-#откуда пиздим
-@client.on(events.NewMessage(-1001708158788)) 
+    @client.on(events.Album(chats=channels))
+    async def Album(event):
+        text = event.original_update.message.message
+        print(text)
+        if not [element for element in Bad_Keys
+                if text.lower().__contains__(element)]:
+            for i in KEYS:
+                text = re.sub(i, KEYS[i], text)
+            try:
+                await client.send_message(
+                    entity=my_channel,
+                    file=event.messages,
+                    message=text + tags,
+                    parse_mode='md',
+                    link_preview=False)
+            except errors.FloodWaitError as e:
+                print(f'[!] Ошибка флуда ждем: {e.seconds} секунд')
+                await asyncio.sleep(e.seconds)
+            except Exception as e:
+                print('[!] Ошибка', e)
 
-#туда срем
-async def main(event):
-    await client.send_message(-1001771993154, event.message)
-    
-client.run_until_disconnected()
+    client.run_until_disconnected()
